e9bd6d12e9aceba05d886acf8fd8d54ef6a87a1f
jssoleey <155762388+jssoleey@users.noreply.github.com>
2025-05-09 09:04:57 +0900

    Update view.py

--- pages/view.py
+++ pages/view.py
@@ -284,7 +284,7 @@ if user_folder and os.path.exists(os.path.join(user_folder, "profile.json")):
                 </div>
             </div>
             """,
-            height=dynamic_height + 35
+            height=dynamic_height + 40
         )
     
     
